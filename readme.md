676a821e45f0348b7e2d5e15fd4dd95ad385a1cc
ellen.carvalho <ellen.carvalho@ws000606.midgard.local>
2017-01-26 15:11:54 -0200

    Concluido

--- readme.md
+++ readme.md
@@ -2,4 +2,7 @@
 Mini projeto de calculo de imc, envolvendo algumas tecnologias:
 * NodeJs
 * Gulp
-* EcmasScript6
+* EcmasScript6 (Babel)
+* React
+* WebPack
+* Sass
--- src/scripts/Formulario.js
+++ src/scripts/Formulario.js
@@ -15,6 +15,7 @@ export default class Formulario extends React.Component{
   handleSubmit(e) {
     e.preventDefault();
 
+    let nome = this.refs.nome.value;
     let imc = calcular(this.state.peso, this.state.altura);
 
     this.refs.nome.value = '';
@@ -22,8 +23,29 @@ export default class Formulario extends React.Component{
     this.refs.peso.value = '';
     this.refs.altura.value = '';
 
+    let conclusao = "";
+    if(imc<18.5){
+      //abaixo do peso
+      conclusao = "Abaixo do peso";
+    } else if(imc >= 18.5 && imc <= 24.9){
+      //peso ideal
+      conclusao = "Peso Ideal!";
+    } else if (imc >= 25 && imc <= 29.9) {
+      //sobrepeso
+      conclusao = "Sobrepeso.";
+    } else if(imc >= 30 && imc <= 34.9){
+      //obesidade I
+      conclusao = "Obesidade I :(";
+    } else if(imc >= 35 && imc <= 39.9){
+      //obesidade II
+      conclusao = "Obesidade II :(";
+    } else if(imc > 40){
+      //obesidade III
+      conclusao = "Obesidade III :(";
+    }
+
     setTimeout(() => {
-      alert(imc);
+      console.log(nome + ", Seu Imc é: "+imc.toFixed(2)+", conclusão: " +conclusao);
     }, 200);
   }
 
@@ -43,37 +65,38 @@ export default class Formulario extends React.Component{
   render(){
     return(
       <div>
+        <center><h1>Descubra seu IMC!</h1></center>
+
         <form onSubmit={::this.handleSubmit}>
-          <label htmlFor="nome">Nome</label>
-          <br />
-          <input type="text" id="nome" defaultValue="" placeholder="Digite seu nome" ref="nome" required/>
-          <br />
-          <label htmlFor="idade">Idade</label>
-          <br />
-          <input type="number" id="idade" defaultValue="" placeholder="Digite sua idade" ref="idade" required/>
-          <br />
-          <label htmlFor="peso">Peso</label>
-          <br />
-          <input type="text"
-                 id="peso"
-                 value={this.state.peso}
-                 onChange={::this.handlePesoChange}
-                 placeholder="Digite seu peso"
-                 ref="peso"
-                 required/>
-          <br />
-          <label htmlFor="altura">Altura</label>
-          <br />
-          <input type="text"
-                 id="altura"
-                 value={this.state.altura}
-                 onChange={::this.handleAlturaChange}
-                 placeholder="Digite sua Altura"
-                 ref="altura"
-                 required/>
-          <br />
-          <br />
-          <input type="submit" id="calcular" value="Descubrir!"/>
+          <div className="40-percent">
+            <label htmlFor="nome">Nome</label>
+            <input type="text" id="nome" defaultValue="" placeholder="Digite seu nome" ref="nome" required/>
+
+            <label htmlFor="idade">Idade</label>
+            <input type="number" id="idade" defaultValue="" placeholder="Digite sua idade" ref="idade" required/>
+
+            <label htmlFor="peso">Peso</label>
+            <input type="text"
+                   id="peso"
+                   value={this.state.peso}
+                   onChange={::this.handlePesoChange}
+                   placeholder="Digite seu peso"
+                   ref="peso"
+                   required/>
+
+            <label htmlFor="altura">Altura</label>
+            <input type="text"
+                   id="altura"
+                   value={this.state.altura}
+                   onChange={::this.handleAlturaChange}
+                   placeholder="Digite sua Altura"
+                   ref="altura"
+                   required/>
+
+            <div className="buttons">
+              <input type="submit" id="calcular" value="Descubrir!"/>
+            </div>
+          </div>
         </form>
       </div>
     );
